c7153c450d381ce94e69e58c0415ebd5dacee943
Diegoarend <diegoaw@gmail.com>
2021-07-03 16:07:32 -0300

    feat: hash password to save on db

--- api/src/modules/users/index.js
+++ api/src/modules/users/index.js
@@ -1,9 +1,11 @@
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import { prisma } from '~/data'
 
 export const login = async (ctx) => {
   try {
     const { email, password } = ctx.request.body
+
     const [user] = await prisma.user.findMany({
       where: { email, password }
     })
@@ -19,7 +21,6 @@ export const login = async (ctx) => {
     ctx.status = 500
     ctx.body = 'algo deu errado'
     return null
-
   }
 }
 
@@ -36,10 +37,16 @@ export const list = async (ctx) => {
   //ctx.body = 'hello usesrs'
 }
 
-export const create = async (ctx, next) => {
+export const create = async (ctx) => {
   try {
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(ctx.request.body.password, saltRounds)
     const user = await prisma.user.create({
-      data: ctx.request.body
+      data: {
+        name: ctx.request.body.name,
+        email: ctx.request.body.email,
+        password: hashedPassword,
+      }
     })
     ctx.body = user
   } catch (error) {
